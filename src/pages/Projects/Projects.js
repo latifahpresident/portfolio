abd0fc1fd88e097efb0a6f4ee3f3ff077b341ed0
Latifah <latifah.pres@gmail.com>
2020-08-09 10:11:06 -0400

    added new project

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -17,6 +17,7 @@ import { Title } from '../../components/component-styles';
 import '../../App.css';
 import lambdaNotesProject from './lambda-notes.png';
 import oerBookr from './OERBookr.png';
+import CHF from './Screen Shot 2020-08-09 at 10.04.19 AM.png';
 
 const Projects = () => {
     return (
@@ -67,6 +68,30 @@ const Projects = () => {
                          </ScrollAnimation>
                          
                     </ProjectDescription>
+                </ProjectContent>
+
+                <ProjectContent>
+                    <ProjectDescription className='project-description'>
+                        <ProjectDescriptionText header>
+                        Cooper’s Home Furniture 
+                        </ProjectDescriptionText>
+                        <ProjectDescriptionText>
+                        Cooper’s Home Furniture is an online furniture store that aims to bring brand new quality
+furniture to the masses. In addition to the e-commerce side of the business, there’s also an
+employee portal where agents can book orders for customers and find important
+information about the business and sales strategies. Accessibility was a driving force of this
+project as the majority of the agents are visually impaired. The site was built using NodeJs,
+Express, PostgreSQL, and Firebase, on the backend and React on the frontend.
+                        </ProjectDescriptionText>
+
+                        <ScrollAnimation animateIn='bounceInDown'>
+                            <ProjectButton href='https://coopershomefurniture.com/'>View Site</ProjectButton>
+                        </ScrollAnimation>
+
+                     </ProjectDescription>           
+                     <Project right reverse>
+                            <ProjectImage src={CHF} alt='coopers home furniture'/>
+                     </Project>
                 </ProjectContent>
             </ProjectsContainer>
         </ProjectsWrapper>
